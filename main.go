10ebb77d059bba1f9bb38722ae49a715b1f9eb47
taqm <warabii.taqm@gmail.com>
2022-04-03 12:07:10 +0900

    add web framework for golang.

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"io"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func main() {
-	fmt.Println("hello world")
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	e := echo.New()
+	setupHandler(e)
+
+	e.Logger.Fatal(e.Start(":" + port))
 }
